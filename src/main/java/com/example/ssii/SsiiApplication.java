513f19619d5544f5a3035af8bc5edf92ca87d072
admin <nourhene.amira@esprit.tn>
2023-09-06 22:15:32 +0100

    ssii

---
+++ src/main/java/com/example/ssii/SsiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssii/SsiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
